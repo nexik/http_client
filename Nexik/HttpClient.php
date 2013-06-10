0e906d766f7efcaab235b03b7b0fdbd02131cc7e
nexik <tomek@sabaki.pl>
2013-06-10 10:20:22 +0200

    Initial commit

---
+++ Nexik/HttpClient.php
@@ -0,0 +1,19 @@
+<?php
+namespace Nexik;
+
+use Goutte\Client;
+
+class HttpClient
+{
+    public function request($url, $method = 'GET', $parameters = array())
+    {
+        if (strpos('/', $url) === 0) {
+            $url = 'http://' . $_SERVER['SERVER_NAME'] . $url;
+        }
+
+        $client = new Client();
+        $client->request($method, $url, $parameters);
+
+        return $client->getResponse()->getContent();
+    }
+}
